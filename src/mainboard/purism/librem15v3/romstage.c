36e11c05387ab2592d679d9053292991642e8339
Youness Alaoui <youness.alaoui@puri.sm>
2017-07-27 11:43:53 -0400

    purism/librem15v3: Add support for Librem 15 v3
    This is a copy of the librem13v2 directory with the following
    changes :
    - Name and version change in Kconfig files
    - Change GPIO A18, A19, A20, D9, D10 and D11 from NC to GPIO
    - Enable PCI device 1c.4
    - Change USB port definitions in devicetree
    
    Change-Id: I572ac654d0eb9c6cb91094f8d150e6a71fb42d70
    Signed-off-by: Youness Alaoui <youness.alaoui@puri.sm>

---
+++ src/mainboard/purism/librem15v3/romstage.c
@@ -0,0 +1,65 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2007-2010 coresystems GmbH
+ * Copyright (C) 2015 Google Inc.
+ * Copyright (C) 2015 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <string.h>
+#include <assert.h>
+#include <arch/acpi.h>
+#include <soc/pei_data.h>
+#include <soc/pei_wrapper.h>
+#include <soc/romstage.h>
+#include <spd_bin.h>
+
+void mainboard_romstage_entry(struct romstage_params *params)
+{
+	/* Fill out PEI DATA */
+	mainboard_fill_pei_data(params->pei_data);
+	/* Initliaze memory */
+	romstage_common(params);
+}
+
+void mainboard_memory_init_params(struct romstage_params *params,
+	MEMORY_INIT_UPD *memory_params)
+{
+	struct spd_block blk = {
+		.addr_map = { 0xa0 },
+	};
+
+	get_spd_smbus(&blk);
+	dump_spd_info(&blk);
+	assert(blk.spd_array[0][0] != 0);
+
+	memory_params->MemorySpdDataLen = blk.len;
+	memory_params->MemorySpdPtr00 = (uintptr_t) blk.spd_array[0];
+	memory_params->MemorySpdPtr01 = 0;
+	memory_params->MemorySpdPtr10 = 0;
+	memory_params->MemorySpdPtr11 = 0;
+
+	memcpy(memory_params->DqByteMapCh0, params->pei_data->dq_map[0],
+		sizeof(params->pei_data->dq_map[0]));
+	memcpy(memory_params->DqByteMapCh1, params->pei_data->dq_map[1],
+		sizeof(params->pei_data->dq_map[1]));
+	memcpy(memory_params->DqsMapCpu2DramCh0, params->pei_data->dqs_map[0],
+		sizeof(params->pei_data->dqs_map[0]));
+	memcpy(memory_params->DqsMapCpu2DramCh1, params->pei_data->dqs_map[1],
+		sizeof(params->pei_data->dqs_map[1]));
+	memcpy(memory_params->RcompResistor, params->pei_data->RcompResistor,
+		sizeof(params->pei_data->RcompResistor));
+	memcpy(memory_params->RcompTarget, params->pei_data->RcompTarget,
+		sizeof(params->pei_data->RcompTarget));
+	memory_params->DqPinsInterleaved = TRUE;
+
+}
